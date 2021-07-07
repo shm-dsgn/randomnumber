bf8da12ba515745a4328d99779aae92aa37c165a
shm-dsgn <77527904+shm-dsgn@users.noreply.github.com>
2021-07-07 11:31:37 +0530

    Final version with complete UI

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function myFunction() {
-  var min = document.getElementById('min').value;
-  var max = document.getElementById('max').value;
-  var x = Math.floor((Math.random() * (max - min + 1)) + min);
-  document.getElementById("demo").innerHTML = x;
+  var min = parseInt(document.getElementById('min').value);
+  var max = parseInt(document.getElementById('max').value);
+  var x = Math.floor(Math.random() * (max - min + 1)) + min;
+  document.getElementById("result").innerHTML = x;
 }
