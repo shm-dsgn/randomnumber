422261dc9196c66fdadea934db643536a6c9c19f
Soham Dutta <77527904+shm-dsgn@users.noreply.github.com>
2021-07-07 12:26:56 +0530

    Bug fixes

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function myFunction() {
   var min = parseInt(document.getElementById('min').value);
   var max = parseInt(document.getElementById('max').value);
-  if (max>min) {
+  if(max==min) {
+    document.getElementById("result").innerHTML = "Both can't be same.";
+  }
+  else if(max>min) {
     var x = Math.floor(Math.random() * (max - min + 1)) + min;
     document.getElementById("result").innerHTML = x;
   } 
